4ef5f8e8c8f3a130f62b8d0427c86854434e17b2
Faith <Faithaikhionbare@outlook.com>
2025-12-20 18:02:24 -0500

    Wrote out the steps for the code in comments at the top so what i am coding

---
+++ log_parser.py
@@ -0,0 +1,8 @@
+#This is the log parser file 
+
+"""Steps for the log parser:
+1. Read the file line by line using the open() function 
+2. for each line. get all the information from each colum
+3. We want to track the failed login attempts using the satus code also track toal requests and supisous ips 
+4. Print output results summary and save supisous logs to separte log file"""
+
